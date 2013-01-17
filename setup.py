1f4aaf0d884838dd08e5e49c31f9ab9f097a47db
David Clark <evil@alphashack.com>
2013-01-17 17:54:43 +1100

    forgot this was a public repo, removed pypi password

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(
 	name='graphdat',
 	version='1.0',
 	description='Graphdat instrumentation module',
+	long_description='Instrument WSGI applications to send performance data back to your graphs at graphdat.com',
 	author='Alphashack',
 	author_email='support@graphdat.com',
 	url='http://www.graphdat.com',
