f0d11c816444f2022aa142d07b7df17207b2af39
David Clark <evil@alphashack.com>
2013-02-20 16:51:14 +1100

    dont use request_uri as it is optional, and not available on django

--- graphdat/wrappers.py
+++ graphdat/wrappers.py
@@ -11,5 +11,7 @@ class WSGIWrapper(object):
     def __call__(self, environ, start_response):
         start_time = time.time()
         ret = self.wrapped(environ, start_response)
-        self.api.store(environ["REQUEST_METHOD"], environ["REQUEST_URI"], environ["SERVER_NAME"], (time.time() - start_time) * 1000)
+	request_uri = '%(scheme)s://%(host)s%(path)s%(query_sep)s%(query)s' % \
+		{'scheme': environ["wsgi.url_scheme"], 'host': environ["HTTP_HOST"], 'path': environ["PATH_INFO"], 'query_sep': '?' if environ["QUERY_STRING"] else '', 'query': environ["QUERY_STRING"]}
+        self.api.store(environ["REQUEST_METHOD"], request_uri, environ["SERVER_NAME"], (time.time() - start_time) * 1000)
         return ret
